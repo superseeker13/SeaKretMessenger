f7cc3d2fa53956c54b523680c8faef80da4af357
Edward Conn <cardseller2@gmail.com>
2019-03-20 20:39:50 -0400

    Initial Commit.

---
+++ app/src/main/java/com/example/seakretmessenger/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.seakretmessenger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+
+        //When send button is pressed
+        public void sendMessage(View view){
+            EditText editText = findViewById(R.id.edit_message);
+            String message = editText.getText().toString();
+        }
+
+}
