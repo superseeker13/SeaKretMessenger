f4f62f4a9c91ad0fba4f716d234f67fe4492b151
Edward Conn <cardseller2@gmail.com>
2019-03-22 17:07:39 -0400

    Working on text sending.  Should work testing.

--- app/src/main/java/com/example/seakretmessenger/TextSender.java
+++ app/src/main/java/com/example/seakretmessenger/TextSender.java
@@ -1,12 +1,47 @@
 package com.example.seakretmessenger;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+/*
+*  Singleton used to send the message to the server over http.
+*  I am not sure at the moment where to store the contact list
+*  or if we should use one at all.
+ */
 
 class TextSender {
     private static final TextSender ourInstance = new TextSender();
+    //private static List contactList = new LinkedList();
 
     private TextSender() {
     }
 
     static TextSender getInstance() {
         return ourInstance;
+    }
+
+    /*
+    * Sends the message given to upstream to be converted to a bitmap.
+    *
+    * //TODO: Add encryption
+     */
+    protected boolean sendMessage(String message){
+        try {
+            URL serverUrl = new URL(message); //Hack FIx
+            HttpURLConnection con = (HttpURLConnection) serverUrl.openConnection();
+            con.setRequestMethod("POST");
+            con.setConnectTimeout(60 * 1000);
+            int status = con.getResponseCode();
+            if(status < 299){
+                con.disconnect();
+                return true;
+            }
+                con.disconnect();
+                return false;
+            } catch (IOException e) {
+                System.err.println(e.toString());
+            }
+        return false;
     }
 }
