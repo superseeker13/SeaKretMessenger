a950f5de84a351ce82f82d4dc72dbbcb80d65c2d
Edward Conn <cardseller2@gmail.com>
2019-03-24 21:04:00 -0400

    Text sender in progress. Fleshed out gui.

--- app/src/main/java/com/example/seakretmessenger/MainActivity.java
+++ app/src/main/java/com/example/seakretmessenger/MainActivity.java
@@ -12,18 +12,23 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_menu);
+        }
+
+        //When sign in button is pressed
+        public void sendLogin(View view){
+            EditText editText_username = findViewById(R.id.edit_username);
+            EditText editText_password = findViewById(R.id.edit_password);
+            final String username = editText_username.getText().toString();
+            final String password = editText_password.getText().toString();
         }
 
 
         //When send button is pressed
         public void sendMessage(View view){
             EditText editText = findViewById(R.id.edit_message);
-            String message = editText.getText().toString();
-            //String recipiet
+            final String message = editText.getText().toString();
         }
-        
-        //TODO: Add message lisener thread and thread stop.
+
         //TODO: Add contact info to message and destination.
-        
 }
