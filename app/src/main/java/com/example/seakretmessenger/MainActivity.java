15295bce99ed0ceecfa95e57d06f6346bb4125c9
Edward Conn <cardseller2@gmail.com>
2019-03-29 18:45:24 -0400

    Implementing threads.

--- app/src/main/java/com/example/seakretmessenger/MainActivity.java
+++ app/src/main/java/com/example/seakretmessenger/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_menu);
+            setContentView(R.layout.activity_main);
         }
 
         //When sign in button is pressed
@@ -21,13 +21,24 @@ public class MainActivity extends AppCompatActivity {
             EditText editText_password = findViewById(R.id.edit_password);
             final String username = editText_username.getText().toString();
             final String password = editText_password.getText().toString();
+            setContentView(R.layout.activity_menu);
         }
 
-
         //When send button is pressed
+        // Lamda creates a Runnable that sends the message.
         public void sendMessage(View view){
             EditText editText = findViewById(R.id.edit_message);
             final String message = editText.getText().toString();
+            Thread sendThread = new Thread(()-> TextSender.getInstance().sendMessage(message));
+        }
+
+        // Lamda creates a Runnable that recieves the message.
+        public void recieveMessage(View view){
+            EditText editText = findViewById(R.id.edit_message);
+            final String message = editText.getText().toString();
+            Thread recieveThread = new Thread(()
+                     -> ImageReceiver.getInstance().getImages("Insert URL"));
+            recieveThread.start();
         }
 
         //TODO: Add contact info to message and destination.
