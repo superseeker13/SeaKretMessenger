89e2b976b60ab21b5fbcbc0bb6a3870f00246bea
Trevor <trevorbelding@gmail.com>
2019-04-14 19:31:27 -0400

    GlideImage stuff
    GLide Imaging for URLs and local files

---
+++ app/src/main/java/com/example/seakretmessenger/GlideImage.java
@@ -0,0 +1,35 @@
+package com.example.seakretmessenger;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+import java.io.File;
+
+public class GlideImage extends AppCompatActivity{
+    private ImageView gifImageView;
+
+    // Loads from URL as a GIF
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.glidetest);
+
+
+        gifImageView = (ImageView) findViewById(R.id.img_gif); // img_gif references xml in glidetest
+
+        loadFromURL();
+        //loadFromFile();
+    }
+
+    // Loads a simple address
+    private void loadFromURL(){
+        String internetURL = "https://techcrunch.com/wp-content/uploads/2015/08/safe_image.gif"; // Enter URL of gif here
+        Glide.with(this).load(internetURL).into(gifImageView);
+    }
+
+    // Loads dog.gif from /app/res/drawable/
+    private void loadFromFile(){
+        Glide.with(this).load(R.drawable.dog).into(gifImageView);
+    }
+}
