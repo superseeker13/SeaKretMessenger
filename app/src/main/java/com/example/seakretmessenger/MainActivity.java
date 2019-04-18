4efed07759898934c26b27059a5f5c8a52809da0
Edward Conn <cardseller2@gmail.com>
2019-04-18 13:08:04 -0400

    Moved methods to proper activitys. Broken.

--- app/src/main/java/com/example/seakretmessenger/MainActivity.java
+++ app/src/main/java/com/example/seakretmessenger/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.seakretmessenger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,18 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    //When sign in button is pressed
-    public void sendLogin(View view){
-        EditText editText_username = findViewById(R.id.edit_username);
-        EditText editText_password = findViewById(R.id.edit_password);
-        username = editText_username.getText().toString();
-        String password = editText_password.getText().toString();
-        Thread sendThread
-                = new Thread(()-> TextSender.getInstance().sendLogin(password, username));
-        sendThread.start();
-        setContentView(R.layout.activity_menu);
+        Button main_activity_button = findViewById(R.id.sign_in_button);
+        main_activity_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText_username = findViewById(R.id.edit_username);
+                EditText editText_password = findViewById(R.id.edit_password);
+                username = editText_username.getText().toString();
+                String password = editText_password.getText().toString();
+                Thread sendThread
+                        = new Thread(()-> TextSender.getInstance().sendLogin(password, username));
+                sendThread.start();
+                Intent startIntent = new Intent( MainActivity.this, ActivityMenu.class);
+            }
+        });
     }
 }
 
